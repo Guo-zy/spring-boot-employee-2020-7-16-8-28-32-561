705dac52d29c77c5863775d99ddc423e5342a0cb
Guo-zy <674402145@qq.com>
2020-07-28 18:29:35 +0800

    [SpringBoot Practice3][Yancy & Alisa] feature: finish Employee service

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeServiceImpl.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeServiceImpl.java
@@ -1,32 +1,42 @@
 package com.thoughtworks.springbootemployee.service;
 
 import com.thoughtworks.springbootemployee.entity.Employee;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements IEmployee{
+
+    private List<Employee> employeeList = new ArrayList<>();
+
     @Override
     public List<Employee> selectAllEmployees() {
-        return null;
+        return employeeList;
     }
 
     @Override
     public Employee selectEmployeeById(int id) {
-        return null;
+        return employeeList.stream().filter(employee -> employee.getId() == id) .findFirst().get();
     }
 
     @Override
     public boolean addEmployee(Employee employee) {
-        return false;
+        return employeeList.add(employee);
     }
 
     @Override
     public boolean deleteEmployeeById(int id) {
-        return false;
+        return employeeList.removeIf(employee -> employee.getId() == id);
     }
 
     @Override
     public boolean updateEmployee(Employee employee) {
-        return false;
+        Employee oldEmployee = employeeList.stream().filter(e -> e.getId() == employee.getId()).findFirst().get();
+        oldEmployee.setName(employee.getName());
+        oldEmployee.setAge(employee.getAge());
+        oldEmployee.setGender(employee.getGender());
+        return true;
     }
 }
